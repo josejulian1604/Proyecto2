22a6eb1f640bdf3abeefa1fff599ffbc056f8399
FranVQ09 <franciscovq@estudiantec.cr>
2022-06-01 17:56:01 -0600

    Agregar KeyListener

--- src/MapaView.java
+++ src/MapaView.java
@@ -4,7 +4,7 @@ import javax.swing.JButton;
 import java.awt.event.*;
 import java.awt.Color;
 
-public class MapaView extends JFrame{
+public class MapaView extends JFrame implements KeyListener{
     public final int HEIGHT = 800;
     public final int WIDTH = 1000;
     public final int CASILLAS = 25;
@@ -19,6 +19,9 @@ public class MapaView extends JFrame{
         boton = new JButton();
         boton.setBounds(0, 0, CASILLA_WIDTH, CASILLA_HEIGHT);
         boton.setText("Boton");
+        panel.add(boton);
+        panel.addKeyListener(this);
+        panel.setFocusable(true);
         panel.setLayout(null);
         panel.setSize(WIDTH, HEIGHT);
         panel.setBackground(Color.darkGray);
@@ -27,7 +30,36 @@ public class MapaView extends JFrame{
         this.setVisible(true);
         this.setSize(WIDTH, HEIGHT);
         this.setResizable(false);
-        panel.add(boton);
         this.add(panel, 0, 0);
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getExtendedKeyCode() == KeyEvent.VK_UP){
+            boton.setLocation(boton.getX(), boton.getY() - 5);
+        }
+        if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN) {
+            boton.setLocation(boton.getX(), boton.getY() + 5);
+        }
+        if (e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
+            boton.setLocation(boton.getX() - 5, boton.getY());
+        }
+        if (e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
+            boton.setLocation(boton.getX() + 5, boton.getY());
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
 }
